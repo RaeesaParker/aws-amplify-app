41c0c4881c101b3cca8e158be44889b353c20851
RaeesaParker <raeesaparker101@gmail.com>
2023-09-02 14:06:31 +0100

    Add amplify imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import Amplify from 'aws-amplify'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
+import config from './aws-exports'
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
+Amplify.configure(config)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
